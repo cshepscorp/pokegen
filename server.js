a6eabd0fa9eade62093e50ecff017814f1194c18
jyanev01 <jordanyanev@gmail.com>
2021-12-01 18:07:58 -0500

    merge mikes API routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const app = express();
+
 // use Heroku's process.env.PORT value when deployed and 3001 when run locally
 const PORT = process.env.PORT || 3001;
 const path = require('path'); 
@@ -15,4 +16,4 @@ app.use(routes);
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
-  });
+});
