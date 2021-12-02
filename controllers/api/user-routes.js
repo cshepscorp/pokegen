24b5d303cfaa65f1795e6bd5547973a5b5086273
cshepscorp <sheppard.christy@gmail.com>
2021-12-01 22:00:31 -0500

    issue with get single user resolved

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,30 +15,27 @@ router.get('/', (req, res) => {
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-    User.findOne({
-        attributes: { exclude: ['password'] },
-        where: {
-          id: req.params.id
-        },
-        include: {
-          model: Post,
-          attributes: ['id', 'post_text', 'user_id', 'created_at']
-        }
-      })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(400).json({ message: 'No user with that email address!' });
-          return;
-        }
-    
-        const validPassword = dbUserData.checkPassword(req.body.password);
-        if (!validPassword) {
-          res.status(400).json({ message: 'Incorrect password!' });
-          return;
-        }
-    
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-      });
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Pokemon,
+      attributes: ['id', 'name', 'type']
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // POST create a new user
