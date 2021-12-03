315b19acde3f425de69b4e317d9b5fcc24fa28d5
jyanev01 <jordanyanev@gmail.com>
2021-12-02 20:43:35 -0500

    added create pokemon handlebars

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path'); 
 
+// const helpers = require('./utils/helpers'); // for help formatting dates
+
+const exphbs = require('express-handlebars');
+
+// pass the helpers to the existing exphbs.create() statement
+const hbs = exphbs.create({ });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); // link static files
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars')
 
 // turn on routes
 app.use(routes);
