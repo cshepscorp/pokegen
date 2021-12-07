895eeeb0f63d9f2379afddc549f27953b55e762b
cshepscorp <sheppard.christy@gmail.com>
2021-12-07 14:07:56 -0500

    commit to merge

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,6 +3,15 @@ async function signupFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
+    if (password.length < 4) {
+        alert('please enter both a username and password (with at least 4 characters');
+        return;
+    }
+
+    // if (response.err.name === 'SequelizeUniqueConstraintError') {
+    //     alert('Sorry. this username is already taken');
+    //     return;
+    // }
     if (username && password) {
         const response = await fetch('/api/users', {
             method: 'post',
