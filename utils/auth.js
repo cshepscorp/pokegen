25ce0dbba3965b8a46d1b94c24fcdea927af9d98
RobertAReedy <robertandreedy@gmail.com>
2021-12-07 15:14:53 -0500

    prevented users from manually entering urls to access other users' data

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,12 @@
 // to make sure a user is logged in
 // To authguard a route means to restrict it to authenticated users only
-/* This function will act as a normal request callback function, checking for the existence of a session property and using res.redirect() if it's not there. If res.session.user_id does exist, it will call next(), which could potentially be another middleware function or the final function that will render the template.*/
+/* This function will act as a normal request callback function,
+checking for the existence of a session property and using res.redirect() if it's not there.
+If res.session.user_id does exist, it will call next(),
+which could potentially be another middleware function or the final function that will render the template.
+*/
+
+const Pokemon = require("../models/Pokemon");
 
 // add withAuth to all non-GET routes to stop unauthorized users from accessing any PUT POST OR DELETE routes
 const withAuth = (req, res, next) => {
