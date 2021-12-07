7dbc7051fc92801d1af36f674a87d6a48c7057c1
RobertAReedy <robertandreedy@gmail.com>
2021-12-07 16:03:09 -0500

    edit-pokemon.handlebars corrected for typos and a mistake in conflict resolution

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,11 +2,10 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Pokemon, User } = require('../models');
 
-// checks if user logged in; have session; authorized
 const withAuth = require('../utils/auth');
 
 // all dashboard views will be prefixed with /dashboard
-router.get('/', withAuth, (req, res) => { // add withAuth here as our own middlware
+router.get('/', withAuth, (req, res) => {
     Pokemon.findAll({
         where: {
           // use the ID from the session so we retrieve posts made by logged in user
@@ -19,11 +18,9 @@ router.get('/', withAuth, (req, res) => { // add withAuth here as our own middlw
         ]
       })
         .then(dbPokemonData => {
-          // serialize data before passing to template
-          // using post here but this can be named ANYTHING 
           const pokemons = dbPokemonData.map(pokemon => pokemon.get({ plain: true }));
-          // user won't be able to get to the dashboard page unless they're logged in
           console.table(pokemons);
+          // user won't be able to get to the dashboard page unless they're logged in
           res.render('dashboard', { pokemons, loggedIn: true, user: req.session.user_id });
         })
         .catch(err => {
@@ -33,7 +30,7 @@ router.get('/', withAuth, (req, res) => { // add withAuth here as our own middlw
 
   });
 
-router.get('/edit/:id', withAuth, (req, res) => { // add withAuth here as our own middlware
+router.get('/edit/:id', withAuth, (req, res) => {
   Pokemon.findOne({
     where: {
         id: req.params.id
